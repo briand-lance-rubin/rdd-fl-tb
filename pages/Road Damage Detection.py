fddcef42e29779ffbbccaade6c5ea7e81572dc68
Briand Rubin <briand.lance.rubin@gmail.com>
2024-11-04 16:40:35 -0500

    Update Road Damage Detection.py

--- pages/Road Damage Detection.py
+++ pages/Road Damage Detection.py
@@ -27,7 +27,6 @@ ROOT = HERE.parent
 logger = logging.getLogger(__name__)
 
 MODEL_URL = "https://github.com/oracl4/RoadDamageDetection/raw/main/models/YOLOv8_Small_RDD.pt"  # noqa: E501
-MODEL_LOCAL_PATH = ROOT / "./models/YOLOv8_Small_RDD.pt"
 download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=89569358)
 
 # Session-specific caching
